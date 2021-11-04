12cb5574d1a3cafae5ada3baa72055fb586658ec
adebadayo <adebayanen@gmail.com>
2021-11-05 00:10:11 +0900

    TODO一覧を状態別に表示させる

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,18 @@ function App() {
 
   console.log("TODOリスト", todoList)
 
+  const inCompletedList = todoList.filter((todo) => {
+    return !todo.done
+  })
+
+  console.log("未完了リスト", inCompletedList)
+
+  const completedList = todoList.filter((todo) => {
+    return todo.done
+  })
+
+  console.log("完了リスト", completedList)
+
   return (
     <>
       <h1>TODO進捗管理</h1>
@@ -27,11 +39,25 @@ function App() {
 
       <button>+TODO追加</button>
 
-      <h2>TODOリスト</h2>
+      <h2>未完了TODOリスト</h2>
       <ul>
-        { todoList.map((todo) => (
+        { inCompletedList.map((todo) => (
             <li key={todo.id}>
-              {todo.content} ({todo.done ? '完了' : '未完了'})
+              {todo.content}
+              <button>{todo.done ? "完了リスト" : "未完了リスト"}</button>
+              <button>削除</button>
+            </li>
+          ))
+        }
+      </ul>
+
+      <h2>完了TODOリスト</h2>
+      <ul>
+        { completedList.map((todo) => (
+            <li key={todo.id}>
+              {todo.content}
+              <button>{todo.done ? "完了リスト" : "未完了リスト"}</button>
+              <button>削除</button>
             </li>
           ))
         }
