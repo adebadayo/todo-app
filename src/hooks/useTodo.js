c250d2b8b6b193d7ea1575b5a107c7e70db28918
adebadayo <adebayanen@gmail.com>
2021-11-05 01:31:46 +0900

    TODOを取得、追加、更新、削除するカスタムフックを作成する

---
+++ src/hooks/useTodo.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react'
+import {ulid} from "ulid";
+
+import * as TodoData from '../api/todo'
+
+export const useTodo = () => {
+
+  const [todoList, setTodoList] = useState([])
+
+  useEffect(() => {
+    TodoData.getAllTodoData().then((todo) => {
+      setTodoList([...todo].reverse())
+    });
+  }, [])
+
+  const toggleTodoListItemState = (id, done) => {
+    const todoItem = todoList.find((todo) => todo.id === id)
+    const newTodoItem = { ...todoItem, done: !done};
+
+    TodoData.updateTodoData(id, newTodoItem).then((updatedTodo) => {
+      const newTodoList = todoList.map((item) =>
+        item.id !== updatedTodo.id ? item : updatedTodo
+      )
+      setTodoList(newTodoList)
+    })
+  }
+
+  const addTodoListItem = (todoContent) => {
+    const newTodoItem = {
+      content: todoContent,
+      id: ulid(),
+      done: false
+    }
+
+    TodoData.addTodoData(newTodoItem).then((addTodo) => {
+      setTodoList([addTodo, ...todoList])
+    })
+  }
+
+  const deleteTodoListItem = (id) => {
+    TodoData.deleteTodoListItem(id).then((deletedItemId) => {
+      const newTodoist = todoList.filter(
+        (item) => item.id !== deletedItemId
+      )
+      setTodoList(newTodoist)
+    })
+  }
+
+
+  return {
+    todoList,
+    toggleTodoListItemState,
+    addTodoListItem,
+    deleteTodoListItem
+  }
+}
