ddf6ac561cc69fa475a4bf767e049383a1a708db
adebadayo <adebayanen@gmail.com>
2021-11-09 01:26:14 +0900

    todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,15 +4,16 @@ import { TodoItem } from './TodoItem'
 import {TodoTitle} from "./TodoTitle";
 
 export const TodoList = ({
+  title,
+  as,
+  fontSize,
   todolist,
   toggleTodoListItemStatus,
   deleteTodoListItem,
-  title,
-  as
 }) => {
   return (
     <>
-      <TodoTitle title={title} as={as}/>
+      <TodoTitle title={title} as={as} fontSize={fontSize} mt="12"/>
       <List w="full">
         {todolist.map((todo) => (
           <TodoItem
